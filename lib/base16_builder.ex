8d649a592c22608e4ac359755f8228ade477b1df
Omar Bahareth <omar@omar.engineer>
2017-04-28 21:06:50 +0300

    Generate Mix project

---
+++ lib/base16_builder.ex
@@ -0,0 +1,18 @@
+defmodule Base16Builder do
+  @moduledoc """
+  Documentation for Base16Builder.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Base16Builder.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/base16_builder_test.exs
@@ -0,0 +1,8 @@
+defmodule Base16BuilderTest do
+  use ExUnit.Case
+  doctest Base16Builder
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end

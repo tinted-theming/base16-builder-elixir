31b119b53c0df21bb0adb99f9162aae4d0eba377
Omar Bahareth <omar@omar.engineer>
2017-09-04 01:09:56 +0300

    Add a module for parsing schemes

---
+++ lib/base_16_builder/scheme.ex
@@ -0,0 +1,38 @@
+defmodule Base16Builder.Scheme do
+  alias Base16Builder.Scheme
+
+  defstruct name: "",
+            slug: "",
+            author: "",
+            bases: %{}
+
+  @bases ~w(
+    base00
+    base01
+    base02
+    base03
+    base04
+    base05
+    base06
+    base07
+    base08
+    base09
+    base0A
+    base0B
+    base0C
+    base0D
+    base0E
+    base0F
+  )
+
+  def from_file(file_path) do
+    yaml = YamlElixir.read_from_file(file_path)
+
+    %Scheme{
+      name: yaml["scheme"],
+      author: yaml["author"],
+      slug: Path.basename(file_path, "yaml") |> Slugger.slugify_downcase,
+      bases: Map.new(@bases, fn(base) -> {base, yaml[base]} end)
+    }
+  end
+end

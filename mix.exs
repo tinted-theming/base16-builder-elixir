40bb63773cf774061929b7fee78746810b24a1e0
Omar Bahareth <omar@omar.engineer>
2017-09-04 14:47:21 +0300

    Replace Mustache.ex with bbmustache
    Mustache.ex rendering is failing due to an issue with it:
    https://github.com/schultyy/Mustache.ex/issues/12

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule Base16Builder.Mixfile do
       {:git_cli, "~> 0.2"},
       {:slugger, "~> 0.2"},
       {:color_utils, "0.2.0"},
-      {:mustache, "~> 0.3.1"},
+      {:bbmustache, "~> 1.5"},
       {:exfmt, [github: "lpil/exfmt"]}
     ]
   end

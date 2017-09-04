c2dbb884a5187acd30dd5e18c16d62ccdcb7bf8d
Omar Bahareth <omar@omar.engineer>
2017-09-04 03:13:58 +0300

    Add Template module for building/rendering

---
+++ lib/base_16_builder/template.ex
@@ -0,0 +1,38 @@
+defmodule Base16Builder.Template do
+  def render(scheme = %Base16Builder.Scheme{}) do
+    nil
+  end
+
+
+  defp build_template_data(scheme = %Base16Builder.Scheme{}) do
+    data = %{
+      "scheme-name" => scheme.name,
+      "scheme-author" => scheme.author,
+      "scheme-slug" => scheme.slug,
+    }
+
+    for {base, value} <- scheme.bases do
+      num_colors = 255
+
+      # Extract R, G, and B color components from Hex
+      rgb =
+        String.upcase(value)
+        |> ColorUtils.hex_to_rgb()
+
+      %{
+        "#{base}-hex-r" => String.slice(value, 0, 2),
+        "#{base}-hex-g" => String.slice(value, 2, 2),
+        "#{base}-hex-b" => String.slice(value, 4, 2),
+        "#{base}-rgb-r" => rgb.red,
+        "#{base}-rgb-g" => rgb.green,
+        "#{base}-rgb-b" => rgb.blue,
+        "#{base}-dec-r" => rgb.red / num_colors,
+        "#{base}-dec-g" => rgb.green / num_colors,
+        "#{base}-dec-b" => rgb.blue / num_colors,
+      }
+    end
+    |> Enum.reduce(data, fn (map, accumulator) ->
+         Map.merge(accumulator, map)
+       end)
+  end
+end

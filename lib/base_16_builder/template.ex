dc996423838ac5d9e51bb964971d54db7769e17c
Omar Bahareth <omar@omar.engineer>
2017-09-04 04:46:47 +0300

    Add render function

--- lib/base_16_builder/template.ex
+++ lib/base_16_builder/template.ex
@@ -1,6 +1,4 @@
 defmodule Base16Builder.Template do
-  def render(scheme = %Base16Builder.Scheme{}) do
-    nil
   alias Base16Builder.Template
   alias Base16Builder.Scheme
 
@@ -16,10 +14,30 @@ defmodule Base16Builder.Template do
       }
     end)
   end
+
+  def render(template = %Template{}, scheme = %Scheme{}) do
+    config = YamlElixir.read_from_file(template.config_file_path)
+
+    for {k, v} <- config do
+      template_data = build_template_data(scheme)
+
+      template_file = Path.join(template.directory, "#{k}.mustache")
+      rendered_filename = "base16-#{scheme.slug}#{v["extension"]}"
+      rendered_dir = Path.join("out", v["output"])
+
+      rendered_template = Mustache.render(File.read(template_file), template_data)
+
+      File.mkdir_p(rendered_dir)
+
+      rendered_file = Path.join(rendered_dir, rendered_filename)
+
+      IO.puts "building #{rendered_file}"
+      File.write(rendered_file, rendered_template)
+    end
   end
 
 
-  defp build_template_data(scheme = %Base16Builder.Scheme{}) do
+  defp build_template_data(scheme = %Scheme{}) do
     data = %{
       "scheme-name" => scheme.name,
       "scheme-author" => scheme.author,
